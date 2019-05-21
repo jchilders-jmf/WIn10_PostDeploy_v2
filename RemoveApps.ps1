944b56410759742cc2a17bd61a9b77fbae4a0381
jchilders-jmf <49322579+jchilders-jmf@users.noreply.github.com>
2019-05-21 09:21:48 -0500

    Update RemoveApps.ps1

--- RemoveApps.ps1
+++ RemoveApps.ps1
@@ -141,3 +141,33 @@ Get-AppxPackage Microsoft.XboxIdentityProvider | Remove-AppxPackage
 # Zune Music, Movies & TV
 Get-AppxPackage Microsoft.ZuneMusic | Remove-AppxPackage
 Get-AppxPackage Microsoft.ZuneVideo | Remove-AppxPackage
+
+Get-AppxPackage "2414FC7A.Viber" | Remove-AppxPackage
+Get-AppxPackage "41038Axilesoft.ACGMediaPlayer" | Remove-AppxPackage
+Get-AppxPackage "46928bounde.EclipseManager" | Remove-AppxPackage
+Get-AppxPackage "4DF9E0F8.Netflix" | Remove-AppxPackage
+Get-AppxPackage "64885BlueEdge.OneCalendar" | Remove-AppxPackage
+Get-AppxPackage "7EE7776C.LinkedInforWindows" | Remove-AppxPackage
+Get-AppxPackage "828B5831.HiddenCityMysteryofShadows" | Remove-AppxPackage
+Get-AppxPackage "89006A2E.AutodeskSketchBook" | Remove-AppxPackage
+Get-AppxPackage "9E2F88E3.Twitter" | Remove-AppxPackage
+Get-AppxPackage "A278AB0D.DisneyMagicKingdoms" | Remove-AppxPackage
+Get-AppxPackage "A278AB0D.MarchofEmpires" | Remove-AppxPackage
+Get-AppxPackage "ActiproSoftwareLLC.562882FEEB491" | Remove-AppxPackage
+Get-AppxPackage "AdobeSystemsIncorporated.AdobePhotoshopExpress" | Remove-AppxPackage
+Get-AppxPackage "CAF9E577.Plex" | Remove-AppxPackage
+Get-AppxPackage "D52A8D61.FarmVille2CountryEscape" | Remove-AppxPackage
+Get-AppxPackage "D5EA27B7.Duolingo-LearnLanguagesforFree" | Remove-AppxPackage
+Get-AppxPackage "DB6EA5DB.CyberLinkMediaSuiteEssentials" | Remove-AppxPackage
+Get-AppxPackage "DolbyLaboratories.DolbyAccess" | Remove-AppxPackage
+Get-AppxPackage "Drawboard.DrawboardPDF" | Remove-AppxPackage
+Get-AppxPackage "Facebook.Facebook" | Remove-AppxPackage
+Get-AppxPackage "flaregamesGmbH.RoyalRevolt2" | Remove-AppxPackage
+Get-AppxPackage "GAMELOFTSA.Asphalt8Airborne" | Remove-AppxPackage
+Get-AppxPackage "KeeperSecurityInc.Keeper" | Remove-AppxPackage
+Get-AppxPackage "king.com.BubbleWitch3Saga" | Remove-AppxPackage
+Get-AppxPackage "king.com.CandyCrushSodaSaga" | Remove-AppxPackage
+Get-AppxPackage "PandoraMediaInc.29680B314EFC2" | Remove-AppxPackage
+Get-AppxPackage "SpotifyAB.SpotifyMusic" | Remove-AppxPackage
+Get-AppxPackage "WinZipComputing.WinZipUniversal" | Remove-AppxPackage
+Get-AppxPackage "XINGAG.XING" | Remove-AppxPackage
